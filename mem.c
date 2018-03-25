1a391f0490497c32a1934e0648c8fb7c5d2933cb
Sarah Depew <sdepew@brynmawr.edu>
2018-03-25 11:52:39 -0400

    Setup

--- mem.c
+++ mem.c
@@ -7,7 +7,17 @@
 #include <sys/mman.h>
 #include "mem.h"
 
+#define SUCCESS 0
+#define ERROR -1
+#define TRUE 1
+#define FALSE 0
+
 int m_error;
+
+//TODO: determine how to track if init has already been called; ask Rachel if solution is acceptable?
+typedef unsigned int boolean;
+
+boolean initialized = FALSE;
 
 typedef struct information {
     //TODO: determine the type of information needed here :)
@@ -28,16 +38,52 @@ typedef struct linkedList {
 static linkedList *free = NULL;
 static linkedList *occupied = NULL;
 
+//helper functions
+size_t roundToPage(int currentSize) {
+    //TODO: implement method that rounds the currentSize to a page size
+    int pageSize = getpagesize();
+    if((currentSize/pageSize)*pageSize == currentSize) {
+        //we are requesting a multiple of page size bytes
+        return (size_t) currentSize;
+    }
+
+    //we do not have a multiple of the page size, yet, so we must round
+    ((((currentSize/pageSize)+1)*pageSize)-pageSize) + pageSize;
+}
+
 int Mem_Init(long sizeOfRegion) {
+    //check sizeOfRegion is a valid, non-negative or zero value
+    if(sizeOfRegion <= 0) {
+        m_error = E_BAD_ARGS;
+        return ERROR;
+    }
+
+    //check that Mem_Init hasn't been called more than once
+    if(initialized == TRUE) {
+        //has already been called once, so this should raise an error
+        m_error = E_BAD_ARGS; //TODO: determine if this is the correct error to call here
+        initialized = TRUE;
+        return ERROR;
+    } else {
+        initialized = TRUE; //we have called this, now
+    }
+
     //Round memory sizeOfRegion to page size using roundToPage //TODO: ask Rachel about what else to do here
 
     //Request that much memory from mmap
 
+    //TODO: check on mmap call here and ask what the heck this is doing...
+    void *mapReturn = mmap(NULL, roundToPage(sizeOfRegion), PROT_EXEC|PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+
+    if(mapReturn == ERROR) {
+        m_error = E_BAD_POINTER; //TODO: check this is correct error to be raising
+        return ERROR;
+    }
+
     //Attach record keeping pointers to the memory //TODO: figure out how to do this...
 
-    //Set E_Bad_Args if needed
-
     //return the appropriate value
+    return SUCCESS;
 }
 
 void *Mem_Alloc(long size);
@@ -45,8 +91,3 @@ void *Mem_Alloc(long size);
 int Mem_Free(void *ptr, int coalesce);
 
 void Mem_Dump();
-
-int roundToPage(int currentSize) {
-    //TODO: implement method that rounds the
-    return 0;
-}
--- mem.h
+++ mem.h
@@ -1,7 +1,7 @@
 #ifndef __MEM_H_
 #define __MEM_H_
 
-#define E_NO_SPACE            1
+#define E_NO_SPACE            1 //TODO: ask Rachel what each of these mean in the context of the project?
 #define E_CORRUPT_FREESPACE   2
 #define E_PADDING_OVERWRITTEN 3
 #define E_BAD_ARGS            4
