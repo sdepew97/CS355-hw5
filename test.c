dfdffcf087606a4ff53ef106c035913243c406cf
Sarah Depew <sdepew@brynmawr.edu>
2018-03-26 13:58:20 -0400

    Mem_Allocate

--- test.c
+++ test.c
@@ -5,7 +5,9 @@ int main() {
     printf("Hello, World!\n");
     Mem_Init(1);
     int *tryingWrite = Mem_Alloc(8);
-
     Mem_Dump();
+    *tryingWrite = 12;
+    Mem_Dump();
+    printf("value of tryingWrite %d\n", *tryingWrite);
     return 0;
 }
