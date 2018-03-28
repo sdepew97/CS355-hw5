f5bd5a1afc78285e5cf3474637910c8d9f0c3a0f
Sarah Depew <sdepew@brynmawr.edu>
2018-03-27 21:28:32 -0400

    Fixing Mem_Init

--- test.c
+++ test.c
@@ -11,16 +11,16 @@ int main() {
 
     printf("Hello, World!\n");
     Mem_Init(1);
-    int *tryingWrite = Mem_Alloc(sizeof(int));
-    Mem_Dump();
-    *tryingWrite = 12;
-    Mem_Dump();
-    printf("value of tryingWrite %d\n", *tryingWrite);
-    int *tryingWrite2 = Mem_Alloc(sizeof(int));
-    Mem_Dump();
-    *tryingWrite2 = 129879807;
-    Mem_Dump();
-    printf("value of tryingWrite2 %d\n", *tryingWrite2);
-    printf("value of memory locations around tryingWrite %d\n", *(tryingWrite2-28));
+//    int *tryingWrite = Mem_Alloc(sizeof(int));
+//    Mem_Dump();
+//    *tryingWrite = 12;
+//    Mem_Dump();
+//    printf("value of tryingWrite %d\n", *tryingWrite);
+//    int *tryingWrite2 = Mem_Alloc(sizeof(int));
+//    Mem_Dump();
+//    *tryingWrite2 = 129879807;
+//    Mem_Dump();
+//    printf("value of tryingWrite2 %d\n", *tryingWrite2);
+//    printf("value of memory locations around tryingWrite %d\n", *(tryingWrite2-28));
     return 0;
 }
