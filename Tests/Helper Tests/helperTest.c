dc3aa6dd21151f4dcc3f19d5fcd4e4b637fe4dae
Sarah Depew <sdepew@brynmawr.edu>
2018-03-31 12:24:35 -0400

    Finished Helper Tests

--- Tests/Helper Tests/helperTest.c
+++ Tests/Helper Tests/helperTest.c
@@ -7,12 +7,12 @@
 #include <unistd.h>
 
 int main() {
-    printf("Round up to page result for odd: %ld\n", roundToPage(1));
-    printf("Round up to page result for aligned: %ld\n", roundToPage(getpagesize()));
+    printf("Round up to page result for odd: %d, %ld\n", 1, roundToPage(1));
+    printf("Round up to page result for aligned: %d, %ld\n", getpagesize(), roundToPage(getpagesize()));
 
-    printf("Round up to word result for odd: %ld\n", roundToWord(13));
-    printf("Round up to word result for aligned: %ld\n", roundToWord(8));
-    printf("Round up to word result for aligned: %ld\n", roundToWord(64));
+    printf("Round up to word result for odd: %d, %ld\n", 13, roundToWord(13));
+    printf("Round up to word result for aligned: %d, %ld\n", 8, roundToWord(8));
+    printf("Round up to word result for aligned: %d, %ld\n", 64, roundToWord(64));
 
     return 0;
 }
