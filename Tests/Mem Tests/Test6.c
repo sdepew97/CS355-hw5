1ba18228047b0308e3cca38cc5bb7e2429d13ab4
Sarah Depew <sdepew@brynmawr.edu>
2018-03-31 22:44:46 -0400

    Added A Lot of Tests

--- Tests/Mem Tests/Test6.c
+++ Tests/Mem Tests/Test6.c
@@ -1,7 +1,55 @@
-//
-// Created by Sarah Depew on 3/26/18.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../list.h"
+#include "../../boolean.h"
+#include "../../helper.h"
+#include "../../mem.h"
+
+
+int test_six() {
+    if (Mem_Init(16) == ERROR) {
+        return EXIT_FAILURE;
+    }
+
+    Mem_Dump();
+
+    void *allocated = Mem_Alloc(1);
+    if (allocated == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    void *allocated2 = Mem_Alloc(7);
+    if (allocated2 == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    void *allocated3 = Mem_Alloc(8);
+    if (allocated3 == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    Mem_Dump();
+    if (Mem_Free(allocated2, FALSE) == ERROR) {
+        return EXIT_FAILURE;
+    }
+    printf("after second freed\n");
+    Mem_Dump();
+
+    if (Mem_Free(allocated, FALSE) == ERROR) {
+        return EXIT_FAILURE;
+    }
+    printf("after first freed\n");
+    Mem_Dump();
+
+    if (Mem_Free(allocated3, TRUE) == ERROR) {
+        return EXIT_FAILURE;
+    }
+    printf("after third freed\n");
+    Mem_Dump();
+
+    return EXIT_SUCCESS;
+}
 
 int main() {
-    return 0;
+    return (test_six());
 }
