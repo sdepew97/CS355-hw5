fbb7d9905f44f5f8e6f9bbdd30a816a7675976b8
Sarah Depew <sdepew@brynmawr.edu>
2018-03-31 20:37:54 -0400

    Testing

--- test.c
+++ test.c
@@ -14,42 +14,42 @@ int main() {
 
     printf("Hello, World!\n");
 
-    if(Mem_Init(16) == ERROR) {
+    if (Mem_Init(16) == ERROR) {
         exit(EXIT_FAILURE);
     }
 
     Mem_Dump();
 
     void *allocated = Mem_Alloc(1);
-    if(allocated == NULL) {
+    if (allocated == NULL) {
         exit(EXIT_FAILURE);
     }
 
     void *allocated2 = Mem_Alloc(7);
-    if(allocated2 == NULL) {
+    if (allocated2 == NULL) {
         exit(EXIT_FAILURE);
     }
 
     void *allocated3 = Mem_Alloc(8);
-    if(allocated3 == NULL) {
+    if (allocated3 == NULL) {
         exit(EXIT_FAILURE);
     }
 
     Mem_Dump();
 //    Mem_Free(allocated, FALSE);
-    if(Mem_Free(allocated2, FALSE) == ERROR) {
+    if (Mem_Free(allocated2, FALSE) == ERROR) {
         exit(EXIT_FAILURE);
     }
     printf("after second freed\n");
     Mem_Dump();
 
-    if(Mem_Free(allocated, FALSE) == ERROR) {
+    if (Mem_Free(allocated, FALSE) == ERROR) {
         exit(EXIT_FAILURE);
     }
     printf("after first freed\n");
     Mem_Dump();
 
-    if(Mem_Free(allocated3, TRUE) == ERROR) {
+    if (Mem_Free(allocated3, TRUE) == ERROR) {
         exit(EXIT_FAILURE);
     }
     printf("after third freed\n");
