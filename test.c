36dd95897bcc6a7c6cd0cb30d1815c350c80f915
Sarah Depew <sdepew@brynmawr.edu>
2018-04-01 09:43:22 -0400

    Test

--- test.c
+++ test.c
@@ -1,59 +1,41 @@
+#include "mem.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
-#include "boolean.h"
-#include "helper.h"
-#include "mem.h"
+#include <time.h>
+#include <assert.h>
+#include <limits.h>
+#include <unistd.h>
+
+#define NUM_ALLOC 2000000
+#define FREE_FREQ 100     // 1:100
+#define BYTE 8
+
+clock_t begin, end;
+
+static void print_execution_time(clock_t begin, clock_t end) {
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Execution time: %.2f seconds\n", time_spent);
+}
+
+void test_two_mil() {
+    int result = Mem_Init(NUM_ALLOC * 40);
+    assert(result == 0);
+
+    void **ptrs = malloc(sizeof(void*) * NUM_ALLOC);
+    for (int i = 0; i < NUM_ALLOC; i++) {
+        ptrs[i] = Mem_Alloc(BYTE);
+        assert(ptrs[i] != NULL);
+        if (i % FREE_FREQ == FREE_FREQ - 1)
+            Mem_Free(ptrs[i-FREE_FREQ+1], 0);
+    }
+
+    end = clock();
+    print_execution_time(begin, end);
+    free(ptrs);
+}
 
 
 int main() {
-    printf("Size of char %ld\n", sizeof(char));
-    printf("Size of long %ld\n", sizeof(long));
-    printf("Size of int %ld\n", sizeof(int));
-    printf("Size of header %ld\n", sizeof(header));
-
-    printf("Hello, World!\n");
-
-    if (Mem_Init(16) == ERROR) {
-        exit(EXIT_FAILURE);
-    }
-
-    Mem_Dump();
-
-    void *allocated = Mem_Alloc(1);
-    if (allocated == NULL) {
-        exit(EXIT_FAILURE);
-    }
-
-    void *allocated2 = Mem_Alloc(7);
-    if (allocated2 == NULL) {
-        exit(EXIT_FAILURE);
-    }
-
-    void *allocated3 = Mem_Alloc(8);
-    if (allocated3 == NULL) {
-        exit(EXIT_FAILURE);
-    }
-
-    Mem_Dump();
-//    Mem_Free(allocated, FALSE);
-    if (Mem_Free(allocated2, FALSE) == ERROR) {
-        exit(EXIT_FAILURE);
-    }
-    printf("after second freed\n");
-    Mem_Dump();
-
-    if (Mem_Free(allocated, FALSE) == ERROR) {
-        exit(EXIT_FAILURE);
-    }
-    printf("after first freed\n");
-    Mem_Dump();
-
-    if (Mem_Free(allocated3, TRUE) == ERROR) {
-        exit(EXIT_FAILURE);
-    }
-    printf("after third freed\n");
-    Mem_Dump();
-
-    exit(EXIT_SUCCESS);
+    test_two_mil();
+    return EXIT_SUCCESS;
 }
