045d3a699a00ef252e066644f929b73437e11bd3
Sarah Depew <sdepew@brynmawr.edu>
2018-04-03 16:07:13 -0400

    Testing with Ziting

--- mem.c
+++ mem.c
@@ -38,7 +38,8 @@ int Mem_Init(long sizeOfRegion) {
     //Request that much memory from mmap
     //TODO: pont define values connected to amountToMMap
 //    long amountToMmap = WORSTCASE;
-    long amountToMmap = AVERAGE;
+//    long amountToMmap = AVERAGE;
+    long amountToMmap = ALIGNED;
 
     void *mapReturn = mmap(NULL, amountToMmap, PROT_EXEC | PROT_READ | PROT_WRITE,
                            MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
