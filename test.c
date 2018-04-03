66c84190c99768a28ef44b6122ca1555c119fcf1
Sarah Depew <sdepew@brynmawr.edu>
2018-04-03 16:34:28 -0400

    Testing with Jiaping

--- test.c
+++ test.c
@@ -1,24 +1,49 @@
 #include "mem.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <assert.h>
-#include <math.h>
-#include <sys/mman.h>
+#include <limits.h>
 #include <unistd.h>
 #include <string.h>
 
+#define NUM_ALLOC 10
+#define FREE_FREQ 100     // 1:100
+#define BYTE 8
+
+clock_t begin, end;
+
+static void print_execution_time(clock_t begin, clock_t end) {
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Execution time: %.2f seconds\n", time_spent);
+}
+
+void test_8_byte() {
+    int result = Mem_Init(8504);
+    assert(result == 0);
+
+    printf("--------------8 byte allocation-----------------\n");
+    char* ptr0 = Mem_Alloc(8);
+    if (ptr0 == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    Mem_Dump();
+    printf("The pointer ptr0 is at address %p.\n", ptr0);
+
+    strcpy(ptr0, "Hello!");
+
+    printf("%s\n", ptr0);
+
+
+    printf("--------------8 byte free-----------------\n");
+    Mem_Free(ptr0, 0);
+    Mem_Dump();
+
+
+}
 
 
 int main() {
-    if(Mem_Init(-10) < 0)
-        printf("Sad, I failed\n");
-    else
-        printf("init successfully\n");
-    printf("The mem_init should fail and we will call Mem_Alloc Mem_Free and Mem_Dump, none of them should work\n");
-    Mem_Dump();
-    void* test = Mem_Alloc(8);
-    assert(test == NULL);
-    assert(Mem_Free(test,1) == -1);
-
-    return 0;
+    test_8_byte();
+    return EXIT_SUCCESS;
 }
